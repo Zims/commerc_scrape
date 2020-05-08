d73aca93eb7b09499fb2948d7077bc7c6be9defe
zims <zims@inbox.lv>
2020-05-08 22:06:59 +0300

    2spiders

--- comerc/comerc/spiders/twentyf.py
+++ comerc/comerc/spiders/twentyf.py
@@ -5,7 +5,21 @@ import scrapy
 class TwentyfSpider(scrapy.Spider):
     name = 'twentyf'
     allowed_domains = ['24.lv']
-    start_urls = ['https://24.lv/datoru-un-biroja-tehnika-en/portatvie-datori-un-piederumi/portatvie-datori/']
+    start_urls = [
+        'https://24.lv/datoru-un-biroja-tehnika-en/portatvie-datori-un-piederumi/portatvie-datori/']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//div[@class='col-tile']"):
+            yield{
+                'name': product.xpath(".//a[@class='product-title']/text()").get(),
+                'price': product.xpath('.//*[@class="ty-price-num"][2]/text()').get()
+
+
+            }
+            
+
+            # yield{
+            #     'title': product.xpath(".//div[@class='specs-wrap']//div[@class='specs js-item-specs']/h3[@class='js-item-title']/text()").get(),
+            #     'price': product.xpath(".//div[@class='specs-wrap']//div[@class='specs js-item-specs']/div[@class=' price']/text()").get()
+
+            # }
--- comerc/comerc/spiders/xnet.py
+++ comerc/comerc/spiders/xnet.py
@@ -5,24 +5,20 @@ import scrapy
 class XnetSpider(scrapy.Spider):
     name = 'xnet'
     allowed_domains = ['xnet.lv']
-    start_urls = ['https://www.xnet.lv/lv/elektronika/portativie-datori/portativie-datori/']
+    start_urls = [
+        'https://www.xnet.lv/lv/elektronika/portativie-datori/portativie-datori/?page=5&perpage=81']
 
     def parse(self, response):
         for product in response.xpath("//a[@class='thumbnail']"):
+
             yield{
                 'title': product.xpath(".//div[@class='specs-wrap']//div[@class='specs js-item-specs']/h3[@class='js-item-title']/text()").get(),
                 'price': product.xpath(".//div[@class='specs-wrap']//div[@class='specs js-item-specs']/div[@class=' price']/text()").get()
+
             }
-            print(product.xpath(".//div[@class='specs-wrap']//div[@class='specs js-item-specs']/h3[@class='js-item-title']/text()").get())
-    # def parse(self, response):
-    #     for product in response.xpath("//div[@class='td-block-span6']"):
-    #         yield {
-    #             'title': product.xpath(".//div[@class='td_module_1 td_module_wrap td-animation-stack']/h3[@class='entry-title td-module-title']/a/text()").get(),
-    #             'url': product.xpath(".//div[@class='td_module_1 td_module_wrap td-animation-stack']/h3[@class='entry-title td-module-title']/a/@href").get(),
+            # 'url': product.xpath(".//div[@class='td_module_1 td_module_wrap td-animation-stack']/h3[@class='entry-title td-module-title']/a/@href").get(),
 
-    #         }
-
-    #         next_page_url = response.xpath('//div[(@class="page-nav td-pb-padding-side")]/a/@href')[-1].extract()
-
+            # next_partial_url = response.xpath(".//ul[@class='pagination']/li[@class]/a[@class='setPage']/@href")[2].extract()
+            # # full_url = 
+            # print(f'I got the response: {next_partial_url}')
     #         yield scrapy.Request(next_page_url, callback=self.parse)
-        
