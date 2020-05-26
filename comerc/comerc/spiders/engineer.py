dc25d7aa3cd53af347dabc64f92bb57acb5d4f99
zims <zims@inbox.lv>
2020-05-26 17:31:27 +0300

    i'm stumped

--- comerc/comerc/spiders/engineer.py
+++ comerc/comerc/spiders/engineer.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import time
 
 
 class EngineerSpider(scrapy.Spider):
     name = 'engineer'
     # allowed_domains = ['www.globalspec.com']
-
+    page_letter = 'A'
     page_num = 1
-    start_urls = ['https://www.globalspec.com/SpecSearch/SuppliersByName/AllSuppliers/A/1']
+    start_urls = [f'https://www.globalspec.com/SpecSearch/SuppliersByName/AllSuppliers/{page_letter}/{page_num}/']
 
 
     def parse(self, response):
@@ -16,12 +17,17 @@ class EngineerSpider(scrapy.Spider):
         # print(urls)
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_details)
-        # if page_num < 10:
 
-        next_page = 'https://www.globalspec.com/SpecSearch/SuppliersByName/AllSuppliers/A/' + str(self.page_num)
-        if self.page_num < 39:
-            self.page_num += 1
-            yield response.follow(next_page, self.parse)
+        # if page_num < 10:
+        # time.sleep(10)
+
+        # next_page = 'https://www.globalspec.com/SpecSearch/SuppliersByName/AllSuppliers/A/' + str(self.page_num) + '/'
+        # print(f'______________________________{next_page}________________________________')
+
+         # if self.page_num < 39:
+            # self.page_num += 1
+            # yield response.follow(next_page, self.parse)
+   
 
 
     def parse_details(self, response):
