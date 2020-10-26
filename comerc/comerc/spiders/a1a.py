049110c19894fc3829fd739b64c86192a1a5185b
zims <zims@inbox.lv>
2020-10-26 11:44:29 +0200

    set up for pics

--- comerc/comerc/spiders/a1a.py
+++ comerc/comerc/spiders/a1a.py
@@ -9,13 +9,13 @@ class A1aSpider(scrapy.Spider):
     start_urls = ['https://www.1a.lv/c/datortehnika-preces-birojam/portativie-datori-un-aksesuari/portativie-datori/2t6?page_per=72&view_mode=grid']
 
     def parse(self, response):
-        for product in response.xpath("//div[@class='new-product-item']"):
+        for product in response.xpath("//div[@class='catalog-taxons-product__hover']"):
             yield{
-                'name': product.xpath(".//a[@class='new-product-name']/text()").get(),
-                'price': product.xpath(".//span[@class='item-price']/span[1]/text()").get(),
-            }
-            part_url = response.xpath("//a[@class='next ']/@href").get()
-            url = response.urljoin(part_url)
+                'name': product.xpath("normalize-space(.//a[@class='catalog-taxons-product__name']/text())").get(),
+                'price': product.xpath("normalize-space(.//span[@class='catalog-taxons-product-price__item-price']/span[1]/text())").get(),
+                }
+        part_url = response.xpath("//a[@class='next ']/@href").get()
+        url = response.urljoin(part_url)
 
-            # print(f'Returned url: {url}')
-            yield scrapy.Request(url, callback=self.parse)
+    # print(f'Returned url: {url}')
+        yield scrapy.Request(url, callback=self.parse)
